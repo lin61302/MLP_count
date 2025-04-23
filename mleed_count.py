f2eb3c363404aff99d8362700397ade108ea710e
ZR <lin61302@gmail.com>
2025-04-23 01:03:32 -0400

    env classifier count (IND) update

--- mleed_count.py
+++ mleed_count.py
@@ -315,7 +315,7 @@ if __name__ == "__main__":
         time.sleep(t)
 
     # Example: just for 'Panama' (PAN)
-    countries_needed = ['ENV_AGO']
+    countries_needed = ['IND']
     # 'PAN','CRI', 'CMR','TUN','LKA','UGA','NPL'
     all_countries = [
         ('Albania', 'ALB'), 
